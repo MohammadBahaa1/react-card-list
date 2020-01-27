9e1eb1dc85898e64586e71bf48f5047048d35068
MohammadBahaa1 <48733136+MohammadBahaa1@users.noreply.github.com>
2020-01-28 01:58:13 +0200

    search-input

--- src/App.js
+++ src/App.js
@@ -22,6 +22,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <input
+          type="search"
+          placeholder="search monster"
+          onChange={e => this.setState({ searchField: e.target.value })}
+        />
         <CardList monesters={this.state.monesters} />
       </div>
     );
